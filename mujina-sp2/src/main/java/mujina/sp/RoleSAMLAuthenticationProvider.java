bda5ec77cbc8f167c3279a8baffc00d81f000e29
xyz-3 <yichen_X1@outlook.com>
2024-07-01 10:43:01 +0800

    multiple sps sso slo

---
+++ mujina-sp2/src/main/java/mujina/sp/RoleSAMLAuthenticationProvider.java
@@ -0,0 +1,16 @@
+package mujina.sp;
+
+import mujina.saml.SAMLPrincipal;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.saml.SAMLAuthenticationProvider;
+import org.springframework.security.saml.SAMLCredential;
+
+import java.util.Collection;
+
+public class RoleSAMLAuthenticationProvider extends SAMLAuthenticationProvider {
+
+    @Override
+    protected Collection<? extends GrantedAuthority> getEntitlements(SAMLCredential credential, Object userDetail) {
+        return ((SAMLPrincipal) userDetail).getAuthorities();
+    }
+}
---
+++ mujina-sp2/src/main/java/mujina/sp/UserController.java
@@ -0,0 +1,68 @@
+package mujina.sp;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    /*
+    如果用户没有登录or登录仍然未失效，Authentication可以获取上下文 -> 非空 -> 重定向到登录后的用户界面；
+    否则，就回到登录界面
+     */
+    @GetMapping("/")
+    public String index(Authentication authentication) {
+        return authentication == null ? "index" : "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(Authentication authentication) {
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return "redirect:/admin.html";
+        } else {
+            return "redirect:/user.html";
+        }
+    }
+
+    @GetMapping({"user", "/user.html"})
+    public String user(Authentication authentication, ModelMap modelMap) {
+        modelMap.addAttribute("user", authentication.getPrincipal());
+        modelMap.addAttribute("authorities", authentication.getAuthorities());
+        return "user";
+    }
+
+    @GetMapping({"admin", "/admin.html"})
+    public String admin(Authentication authentication, ModelMap modelMap) {
+        if(authentication == null) {
+            return "redirect:/";
+        }
+        if(authentication.getAuthorities().stream().noneMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return "redirect:/home";
+        }
+        modelMap.addAttribute("user", authentication.getPrincipal());
+        return "admin";
+    }
+
+    @GetMapping({"page1", "/page1.html"})
+    public String page1(Authentication authentication, ModelMap modelMap) {
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_SVIP"))) {
+            modelMap.addAttribute("user", authentication.getPrincipal());
+            return "page1";
+        } else {
+            return "redirect:/home";
+        }
+    }
+
+    @GetMapping({"page2", "/page2.html"})
+    public String page2(Authentication authentication, ModelMap modelMap) {
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_VIP"))) {
+            modelMap.addAttribute("user", authentication.getPrincipal());
+            return "page2";
+        } else {
+            return "redirect:/home";
+        }
+    }
+
+}
